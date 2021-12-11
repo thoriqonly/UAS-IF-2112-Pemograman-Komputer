fb1e0cc2267a2d0f3546f8b00813640b8b698ce2
thoriqonly <95929338+thoriqonly@users.noreply.github.com>
2021-12-11 23:06:14 +0700

    Update soal1d.py

--- apps/soal1d.py
+++ apps/soal1d.py
@@ -14,67 +14,61 @@ import plotly.express as px
 import json
 
 
-excel_file = 'produksi_minyak_mentah.csv'
+def app():
+    excel_file = 'produksi_minyak_mentah.csv'
 
-st.write('# Soal 1 D')
+    st.write('# Soal 1 D')
 
-with open('kode_negara_lengkap.json') as f:
-        data = json.load(f)
-        names = dict()
-        region = dict()
-        alpha3 = dict()
-        subregion = dict()
-        for x in data:
-            country = x['name']
-            idc = x['alpha-3']
-            names[idc] = country
-            alpha3[idc] = x['country-code']
-            region[idc] = x['region']
-            subregion[idc] = x['sub-region']
-
-
-            
+    with open('kode_negara_lengkap.json') as f:
+            data = json.load(f)
+            names = dict()
+            region = dict()
+            alpha3 = dict()
+            subregion = dict()
+            for x in data:
+                country = x['name']
+                idc = x['alpha-3']
+                names[idc] = country
+                alpha3[idc] = x['country-code']
+                region[idc] = x['region']
+                subregion[idc] = x['sub-region']
 
 
-df2 = pd.read_csv(excel_file)
-df2['produksi'].nlargest(1)
+                
 
 
-negaraminyak = df2['kode_negara'].unique()
-minyak_selection = st.selectbox('Pilih :', negaraminyak)
-
-st.write("Negara yang dipilih : ",names[minyak_selection])
-st.write("Kode Negara: ",alpha3[minyak_selection])
-st.write("Region :",region[minyak_selection])
-st.write("Sub Region :",subregion[minyak_selection])
-
-filterbanyak = df2[df2.kode_negara== minyak_selection]
-filterbanyak = filterbanyak.nlargest(1, columns='produksi')
-
-filtersedikit = df2[df2.kode_negara== minyak_selection]
-filtersedikit = filtersedikit.nsmallest(44, columns='produksi')
-newfilter = filtersedikit.loc[filtersedikit[filtersedikit.produksi > 0].groupby(by='kode_negara')['produksi'].idxmin()]
-newfilter = newfilter.nsmallest(1, columns='produksi')
+    df2 = pd.read_csv(excel_file)
+    df2['produksi'].nlargest(1)
 
 
-seluruhtahun = df2[df2.kode_negara== minyak_selection]
+    negaraminyak = df2['kode_negara'].unique()
+    minyak_selection = st.selectbox('Pilih :', negaraminyak)
 
-st.markdown("<h3 style='text-align: left; color: white;'>Tahun Terbanyak</h3>", unsafe_allow_html=True)
-if filterbanyak['produksi'].unique() == 0:
-    st.write('Tidak ada produksi')
-else:
-    st.dataframe(filterbanyak)
-st.markdown("<h3 style='text-align: left; color: white;'>Tahun Tersedikit</h3>", unsafe_allow_html=True)
-if newfilter.empty:
-    st.write('Tidak ada produksi')
-else:
-    st.dataframe(newfilter)
-st.markdown("<h3 style='text-align: left; color: white;'>Produksi Seluruh Tahun</h3>", unsafe_allow_html=True)
-st.dataframe(seluruhtahun)
+    st.write("Negara yang dipilih : ",names[minyak_selection])
+    st.write("Kode Negara: ",alpha3[minyak_selection])
+    st.write("Region :",region[minyak_selection])
+    st.write("Sub Region :",subregion[minyak_selection])
 
+    filterbanyak = df2[df2.kode_negara== minyak_selection]
+    filterbanyak = filterbanyak.nlargest(1, columns='produksi')
 
-
+    filtersedikit = df2[df2.kode_negara== minyak_selection]
+    filtersedikit = filtersedikit.nsmallest(44, columns='produksi')
+    newfilter = filtersedikit.loc[filtersedikit[filtersedikit.produksi > 0].groupby(by='kode_negara')['produksi'].idxmin()]
+    newfilter = newfilter.nsmallest(1, columns='produksi')
 
 
+    seluruhtahun = df2[df2.kode_negara== minyak_selection]
 
-
+    st.markdown("<h3 style='text-align: left; color: white;'>Tahun Terbanyak</h3>", unsafe_allow_html=True)
+    if filterbanyak['produksi'].unique() == 0:
+        st.write('Tidak ada produksi')
+    else:
+        st.dataframe(filterbanyak)
+    st.markdown("<h3 style='text-align: left; color: white;'>Tahun Tersedikit</h3>", unsafe_allow_html=True)
+    if newfilter.empty:
+        st.write('Tidak ada produksi')
+    else:
+        st.dataframe(newfilter)
+    st.markdown("<h3 style='text-align: left; color: white;'>Produksi Seluruh Tahun</h3>", unsafe_allow_html=True)
+    st.dataframe(seluruhtahun)
